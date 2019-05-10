fa58fde3cf7f8be047e3e8374f536525e61c80f1
ravi-raj-97 <raviraj97@gmail.com>
2019-05-11 02:09:48 +0530

    Webapp completed. Need to integrate with the scripts.

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash, send_from_directory, send_file
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/home/chraviraj/Desktop/Frontend/uploaded_recordings'
+ALLOWED_EXTENSIONS = set(['wav'])
+
 app = Flask(__name__,static_url_path = '/static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'hello'
 
 name_list=[]
 pass_list=[]
 cons_list=[]
 speaker_num=0
+
+track="/home/chraviraj/Desktop/Frontend/infinity_war.mp3"
 
 with open('namelist.txt') as nf:
 	for line in nf:
@@ -16,6 +26,14 @@ with open('namelist.txt') as nf:
 		temp=[speaker_num,row[2]]
 		pass_list.append(temp)
 nf.close()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/playSong')
+def playSong():
+    return track.play()
 
 @app.route('/#home')
 @app.route('/')
@@ -33,13 +51,24 @@ def generate():
 @app.route('/#contact')
 def contact():
 	return render_template('index.html',names=name_list)
-@app.route('/upload',methods=['GET','POST'])
+
+@app.route('/upload',methods=['POST'])
 def upload():
 	if request.method=='POST':
-		filepath=request.form['wavpath']
+		filename=''
 		speakername=request.form['speakername']
 		speakerpass=request.form['speakerpass']
-	print(filepath,speakername,speakerpass)
+		if 'file' not in request.files:
+			flash('No file selected')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+	print(filename,speakername,speakerpass)
+	tpath=UPLOAD_FOLDER+'/'+speakername
 	flag=0
 	count=0
 	for row in name_list:
@@ -48,19 +77,43 @@ def upload():
 			flag=1
 			serialnum=row[0]
 			if pass_list[serialnum-1][1] != speakerpass:
-				errmsg="Special Key Error"
+				flash('Special Key Error')
 				return redirect('/#train')
 	if flag==0:
 		count+=1
 		name_list.append([count,speakername])
 		pass_list.append([count,speakerpass])
+		os.mkdir(tpath)
 		with open('namelist.txt','a') as nf:
 			newline=str(count)+","+speakername+","+speakerpass+"\n"
 			nf.write(newline)
 		nf.close()
-		print('what shit')
-	print("reached successfully")
+	file.save(os.path.join(tpath, filename))
+	flash(filename+' uploaded succesfully')
 	return redirect('/#generate')
+
+@app.route('/synthesizer',methods=['POST'])
+def syn():
+	if request.method=='POST':
+		speakername=request.form['speakername']
+		textcontent=request.form['converttext']
+		speakerpass=request.form['speakerpass']
+		count=0
+		for row in name_list:
+			count+=1
+			if speakername == row[1]:
+				serialnum=row[0]
+				if pass_list[serialnum-1][1] != speakerpass:
+					flash('Special Key invalid. Please re-enter.')
+					return redirect('/#generate')
+		flash('The uploaded text is synthesized in the voice of '+speakername+'.')
+		flash('Click Play or Download now.')
+	return redirect('/#generate') 
+
+
+@app.route('/download-file')
+def down():
+	return send_file('/home/chraviraj/Desktop/Frontend/uploaded_recordings/infinity_war.mp3',attachment_filename="infinity_war.mp3")
 
 @app.route('/alphabet.txt')
 def openfile():
