6399a33767ff36daee4abf65fa22e08130511e04
ravi-raj-97 <raviraj97@gmail.com>
2019-05-12 01:12:21 +0530

    Integration completed

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, request, url_for, flash, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 import os
+import create_soundlist as cs
+import play_words_2 as pw2
+import re
 
-UPLOAD_FOLDER = '/home/chraviraj/Desktop/Frontend/uploaded_recordings'
+UPLOAD_FOLDER = '/home/chraviraj/voicesyn/uploaded_recordings'
 ALLOWED_EXTENSIONS = set(['wav'])
 
 app = Flask(__name__,static_url_path = '/static')
@@ -27,41 +30,30 @@ with open('namelist.txt') as nf:
 		pass_list.append(temp)
 nf.close()
 
-
 def allowed_file(filename):
-	return '.' in filename and  filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/playSong')
 def playSong():
     return track.play()
 
-
 @app.route('/#home')
 @app.route('/')
 def home():
 	return render_template('index.html',names=name_list)
-
-
 @app.route('/#about')
 def about():
 	return render_template('index.html',names=name_list)
-
-
 @app.route('/#train',methods=['GET','POST'])
 def train():
 	return render_template('index.html',names=name_list)
-
-
 @app.route('/#generate')
 def generate():
 	return render_template('index.html',names=name_list)
-
-
 @app.route('/#contact')
 def contact():
 	return render_template('index.html',names=name_list)
-
 
 @app.route('/upload',methods=['POST'])
 def upload():
@@ -69,39 +61,49 @@ def upload():
 		filename=''
 		speakername=request.form['speakername']
 		speakerpass=request.form['speakerpass']
-		if 'file' not in request.files:
-			flash('No file selected')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-	print(filename,speakername,speakerpass)
-	tpath=UPLOAD_FOLDER+'/'+speakername
-	flag=0
-	count=0
-	for row in name_list:
-		count+=1
-		if speakername == row[1]:
-			flag=1
-			serialnum=row[0]
-			if pass_list[serialnum-1][1] != speakerpass:
-				flash('Special Key Error')
-				return redirect('/#train')
-	if flag==0:
-		count+=1
-		name_list.append([count,speakername])
-		pass_list.append([count,speakerpass])
-		os.mkdir(tpath)
-		with open('namelist.txt','a') as nf:
-			newline=str(count)+","+speakername+","+speakerpass+"\n"
-			nf.write(newline)
-		nf.close()
-	file.save(os.path.join(tpath, filename))
-	flash(filename+' uploaded succesfully')
-	return redirect('/#generate')
+		x=request.files.getlist('files[]')
+		uploadedfiles=[]
+		print(x)
+		for file in x:
+			if file.filename == '':
+				flash('No selected file')
+				return redirect(request.url)
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+			print(filename,speakername,speakerpass)
+			totalpath=UPLOAD_FOLDER+'/'+speakername
+			flag=0
+			count=0
+			for row in name_list:
+				count+=1
+				if speakername == row[1]:
+					flag=1
+					serialnum=row[0]
+					if pass_list[serialnum-1][1] != speakerpass:
+						flash('Special Key Error')
+						return redirect('/#train')
+			if flag==0:
+				count+=1
+				name_list.append([count,speakername])
+				pass_list.append([count,speakerpass])
+				os.mkdir(totalpath)
+				os.mkdir(totalpath+'/diphones')
+				with open('namelist.txt','a') as nf:
+					newline=str(count)+","+speakername+","+speakerpass+"\n"
+					nf.write(newline)
+				nf.close()
+			file.save(os.path.join(totalpath, filename))
+			filepath='uploaded_recordings/'+speakername+'/'+filename
+			uploadedfiles.append(filepath)
+			print(uploadedfiles)
+			flash(filename+' uploaded and extracted succesfully.')
+			fne,ext=filename.split('.')
+			print(fne)
+			textpath='uploaded_recordings/'+fne+'.txt'
+			folderpath='uploaded_recordings/'+speakername+'/diphones'
+			cs.generate_diphones(filepath,textpath,folderpath)
+		#audio_file, transcript_file, output_folder, pre_padding=0.0, post_padding=0.0)
+		return redirect('/#train')
 
 @app.route('/synthesizer',methods=['POST'])
 def syn():
@@ -117,18 +119,22 @@ def syn():
 				if pass_list[serialnum-1][1] != speakerpass:
 					flash('Special Key invalid. Please re-enter.')
 					return redirect('/#generate')
+		folderpath='uploaded_recordings/'+speakername+'/diphones'
+		res=re.findall(r'\w+',textcontent)
+		print(res)
+		pw2.output(folderpath,res,'split.txt',0.01,0.02,'generated.wav')
+		#source_folder, word_list, dict_file, diphone_silence=.01, word_silence=.2, name=None
 		flash('The uploaded text is synthesized in the voice of '+speakername+'.')
 		flash('Click Play or Download now.')
 	return redirect('/#generate') 
 
-
-@app.route('/download-file')
+@app.route('/generatedfile')
 def down():
-	return send_file('/home/chraviraj/Desktop/Frontend/uploaded_recordings/infinity_war.mp3',attachment_filename="infinity_war.mp3")
+	return send_file('/home/chraviraj/voicesyn/generated.wav',attachment_filename="generated")
 
-@app.route('/alphabet.txt')
+@app.route('/transcript.txt')
 def openfile():
-	text = open('alphabet.txt', 'r+')
+	text = open('transcript.txt', 'r+')
 	content = text.read()
 	text.close()
 	return render_template('fileview.html', text=content)
